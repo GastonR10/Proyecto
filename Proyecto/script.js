446edee1d3bf3cdfab0794fe85c1addc1affc8bb
Gaston Roberto <robertga@basfad.basf.net>
2021-11-23 21:21:20 -0300

    link login con backend

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -117,4 +117,68 @@ const actualizaInfo = (maximoCaracteres) => {
 
 }
 
+// const button = document.getElementById("submitComment");
+
+// const commentElement = document.getElementById("text");
+
+// button.addEventListener("click", ()=> {
+//   const comment = commentElement.value;
+// })
+
+// const newComment = { comment: comment };
+
+// fetch("", {
+//   method: "POST",
+
+//   body: JSON.stringify(newComment),
+// })
+// .then(function (response) {
+//   return response.json();
+// })
+
+
+// función botón login
+
+const button = document.getElementById("login");
+
+const mailElement = document.getElementById("mail");
+const passwordElement = document.getElementById("password");
+
+button.addEventListener("click", () => {
+  const mail = mailElement.value;
+  const password = passwordElement.value;
+
+  if (mail && password) {
+    const objetoBody = {
+      mail: mail,
+      password: password,
+    };
+
+    fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objetoBody),
+    })
+
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        debugger;
+
+        if (!response.error) {
+          localStorage.setItem("token", response.token);
+
+          alert("Login exitoso");
+        } else {
+          alert("Login fallido");
+        }
+      });
+  } else {
+    alert("Falta completar campos");
+  }
+});
+
 
